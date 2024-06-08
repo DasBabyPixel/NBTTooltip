324652655ab7a0f73c2a7a82ed83d12bb3291efb
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2024-06-08 19:59:10 +0200

    return to tabs

--- src/main/java/zabi/minecraft/nbttooltip/config/TriggerType.java
+++ src/main/java/zabi/minecraft/nbttooltip/config/TriggerType.java
@@ -1,16 +1,14 @@
 package zabi.minecraft.nbttooltip.config;
 
 import java.util.function.BiFunction;
+
 import net.minecraft.client.item.TooltipType;
 import net.minecraft.item.Item;
 import zabi.minecraft.nbttooltip.NBTTooltip;
 
 public enum TriggerType {
 
-	F3H((ctx, type) -> type.isAdvanced()),
-	ALWAYS_ON((ctx, type) -> true),
-	TOGGLE_ON_KEY((ctx, type) -> NBTTooltip.nbtKeyToggled),
-	SHOW_ON_KEY((ctx, type) -> NBTTooltip.nbtKeyPressed);
+	F3H((ctx, type) -> type.isAdvanced()), ALWAYS_ON((ctx, type) -> true), TOGGLE_ON_KEY((ctx, type) -> NBTTooltip.nbtKeyToggled), SHOW_ON_KEY((ctx, type) -> NBTTooltip.nbtKeyPressed);
 
 	private BiFunction<Item.TooltipContext, TooltipType, Boolean> test;
 
